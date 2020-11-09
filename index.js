8df15706a53d6f3ac8e67c755f4aff4e4fce90c4
Matt Kinnersley <kinnersleymatt@gmail.com>
2020-11-09 13:54:50 +0000

    Create Asana Github Integration
    Prior to this change, code was hosted in another repo.
    
    This change adds some tests too.

--- index.js
+++ index.js
@@ -1,21 +1,132 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const asana = require("asana");
 
+const initAsana = ({ asanaToken }) => {
+  return asana.Client.create().useAccessToken(asanaToken);
+};
+const initOctokit = ({ githubToken }) => {
+  return github.getOctokit(githubToken);
+};
 
-// most @actions toolkit packages have async methods
-async function run() {
+const getAsanaTask = async ({ gid, client }) => {
+  const task = await client.tasks.getTask(gid);
+  if (!task) {
+    throw new Error(`Task not found with gid: ${gid}`);
+  }
+  return task;
+};
+
+const getAsanaTaskUrl = async ({ gid, client }) => {
+  const { permalink_url } = await getAsanaTask({ gid, client });
+  if (!permalink_url) {
+    throw new Error(`Task URL not found for gid: ${gid}`);
+  }
+  return permalink_url;
+};
+
+const addPRToAsanaTask = async ({ gid, prUrl, client }) => {
+  const comment = `GitHub PR: ${prUrl}`;
+  return await client.stories.createStoryForTask(gid, { text: comment });
+};
+
+const commentOnIssue = async ({
+  asanaTaskUrl,
+  owner,
+  repo,
+  issue_number,
+  client,
+}) => {
+  const comment = `Asana Task: ${asanaTaskUrl}`;
+  return await client.issues.createComment({
+    owner,
+    repo,
+    issue_number,
+    body: comment,
+  });
+};
+
+const getAsanaTaskGid = ({ ref }) => {
+  const lastSlashIndex = ref.lastIndexOf("/");
+  if (lastSlashIndex == -1) {
+    throw new Error(`Could not find slash in ref: ${ref}`);
+  }
+  return ref.substring(lastSlashIndex + 1);
+};
+
+const getGithubDetails = ({
+  context: {
+    payload: {
+      pull_request: {
+        head: { ref },
+        html_url: prUrl,
+        number: issue_number,
+      },
+      repository: { full_name },
+    },
+  },
+}) => {
+  console.log(ref, prUrl, issue_number, full_name);
+  validateGithubDetails([
+    { name: "pr ref", value: ref },
+    { name: "pr url", value: prUrl },
+    { name: "issue number", value: issue_number },
+    { name: "repo name", value: full_name },
+  ]);
+  return { ref, prUrl, issue_number, full_name };
+};
+
+const validateGithubDetails = (properties) => {
+  const undefinedProperties = [];
+  properties.forEach((property) => {
+    if (!property.value) {
+      undefinedProperties.push(property.name);
+    }
+  });
+  if (undefinedProperties.length > 0) {
+    throw new Error(
+      `Cannot find the following properties: ${undefinedProperties.join(", ")}`
+    );
+  }
+};
+
+const run = async () => {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const asanaToken = core.getInput("asana-token");
+    const githubToken = core.getInput("github-token");
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const asanaClient = initAsana({ asanaToken });
+    const octoKitClient = initOctokit({ githubToken });
 
-    core.setOutput('time', new Date().toTimeString());
+    const { ref, prUrl, issue_number, full_name } = getGithubDetails(github);
+
+    const [owner, repo] = full_name.split("/");
+
+    const gid = getAsanaTaskGid({ ref });
+
+    await addPRToAsanaTask({
+      gid,
+      prUrl,
+      client: asanaClient,
+    });
+
+    const asanaTaskUrl = await getAsanaTaskUrl({ gid, client: asanaClient });
+
+    await commentOnIssue({
+      asanaTaskUrl,
+      owner,
+      repo,
+      issue_number,
+      client: octoKitClient,
+    });
+
+    core.setOutput("pr_url", prUrl);
+    core.setOutput("asana_task_url", asanaTaskUrl);
   } catch (error) {
     core.setFailed(error.message);
   }
-}
+};
 
 run();
+
+module.exports = { getAsanaTaskGid, getGithubDetails, getAsanaTask };
--- index.test.js
+++ index.test.js
@@ -1,23 +1,99 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+const {
+  getAsanaTaskGid,
+  getGithubDetails,
+  getAsanaTask,
+} = require("./index.js");
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+const mockGid = "12345678910";
+
+const mockRef = `some-new-feature/${mockGid}`;
+const mockInvalidRef = `some-new-feature-${mockGid}`;
+
+const mockOwner = "cool-owner";
+const mockRepo = "cool-asana-repo";
+const mockIssueNumber = "10";
+const mockPrUrl = `https://github.com/${mockOwner}/${mockRepo}/pull/${mockIssueNumber}`;
+const mockRepoFullName = `${mockOwner}/${mockRepo}`;
+
+const mockGithubData = {
+  context: {
+    payload: {
+      pull_request: {
+        head: { ref: mockRef },
+        html_url: mockPrUrl,
+        number: mockIssueNumber,
+      },
+      repository: { full_name: mockRepoFullName },
+    },
+  },
+};
+
+const mockInvalidGithubData = {
+  context: {
+    payload: {
+      pull_request: {
+        head: { ref: "" },
+        html_url: mockPrUrl,
+        number: mockIssueNumber,
+      },
+      repository: { full_name: "" },
+    },
+  },
+};
+
+const mockAsanaClient = {
+  tasks: {
+    getTask: jest.fn((x) => x),
+  },
+  stories: {
+    createStoryForTask: jest.fn(),
+  },
+};
+
+describe("getAsanaTaskGid", () => {
+  it("should extract asana task id from branch", async () => {
+    await expect(getAsanaTaskGid({ ref: mockRef })).toBe(mockGid);
+  });
+  it("should throw error with invalid ref structure", async () => {
+    await expect(() => getAsanaTaskGid({ ref: mockInvalidRef })).toThrow(
+      `Could not find slash in ref: ${mockInvalidRef}`
+    );
+  });
 });
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
+describe("getGithubDetails", () => {
+  it("should extract correct details from github context", async () => {
+    const { ref, prUrl, issue_number, full_name } = getGithubDetails(
+      mockGithubData
+    );
+    expect(ref).toBe(mockRef);
+    expect(prUrl).toBe(mockPrUrl);
+    expect(issue_number).toBe(mockIssueNumber);
+    expect(full_name).toBe(mockRepoFullName);
+  });
+
+  it("should throw error if github context does not have one of required properties", async () => {
+    expect(() => getGithubDetails(mockInvalidGithubData)).toThrow(
+      `Cannot find the following properties: pr ref, repo name`
+    );
+  });
 });
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
-})
+describe("getAsanaTask", () => {
+  it("should get asana task with given id", async () => {
+    await getAsanaTask({
+      gid: mockGid,
+      client: mockAsanaClient,
+    });
+    expect(mockAsanaClient.tasks.getTask).toBeCalled();
+  });
+  it("should throw error when not given id", async () => {
+    let error;
+    try {
+      await getAsanaTask({ gid: "", client: mockAsanaClient });
+    } catch (e) {
+      error = e;
+    }
+    expect(error).toEqual(new Error("Task not found with gid: "));
+  });
+});
